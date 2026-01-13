ddfdc4fb8fd0423a33d7763d04309882e1c74479
Denis Shulga <sulgadenis4@gmail.com>
2026-01-13 18:18:49 +0100

    fix: make auth celebrate validation consistent via schemas

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,30 +1,31 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 
-import { resetPassword } from '../controllers/authController.js';
-import { resetPasswordSchema } from '../validations/authValidation.js';
-
 import {
   registerUser,
   loginUser,
   refreshUserSession,
   logoutUser,
+  requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 
-import { registerUserSchema, loginUserSchema } from '../validations/authValidation.js';
-
-import { requestResetEmail } from '../controllers/authController.js';
-import { requestResetEmailSchema } from '../validations/authValidation.js';
+import {
+  registerUserSchema,
+  loginUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validations/authValidation.js';
 
 const router = Router();
 
-router.post('/register', celebrate({ body: registerUserSchema }), registerUser);
-router.post('/login', celebrate({ body: loginUserSchema }), loginUser);
+router.post('/register', celebrate(registerUserSchema), registerUser);
+router.post('/login', celebrate(loginUserSchema), loginUser);
+router.post('/refresh', refreshUserSession);
+router.post('/logout', logoutUser);
 
 router.post('/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
 
-router.post('/refresh', refreshUserSession);
-router.post('/logout', logoutUser);
 router.post('/reset-password', celebrate(resetPasswordSchema), resetPassword);
 
 export default router;
--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,15 +1,19 @@
 import Joi from 'joi';
 import { Segments } from 'celebrate';
 
-export const registerUserSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
-});
+export const registerUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  }),
+};
 
-export const loginUserSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-});
+export const loginUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+};
 
 export const requestResetEmailSchema = {
   [Segments.BODY]: Joi.object({
